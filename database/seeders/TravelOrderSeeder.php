ace758a777e8c60ec70dc4a21971bf77b7171208
Davi Quaresma <quaresma.davi@gmail.com>
2026-03-17 15:36:58 -0300

    feat: create TravelOrderSeeder to generate travel orders for test users

---
+++ database/seeders/TravelOrderSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\TravelOrderStatus;
+use App\Models\TravelOrder;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TravelOrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Criar usuários de teste
+        $users = User::factory(5)->create();
+
+        // Criar pedidos de viagem para cada usuário
+        foreach ($users as $user) {
+            // Pedidos solicitados
+            TravelOrder::factory(3)
+                ->forUser($user)
+                ->requested()
+                ->create();
+
+            // Pedidos aprovados
+            TravelOrder::factory(2)
+                ->forUser($user)
+                ->approved()
+                ->create();
+
+            // Pedidos cancelados
+            TravelOrder::factory(1)
+                ->forUser($user)
+                ->canceled()
+                ->create();
+        }
+    }
+}
